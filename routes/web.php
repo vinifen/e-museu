83ad5e490bc4b9299079762ad6dd4a122e70cf15
tankesho <takeoga@hotmail.com>
2024-06-11 20:39:36 -0300

    Fixed https configuration in web file

--- routes/web.php
+++ routes/web.php
@@ -154,3 +154,5 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+URL::forceScheme('https');
