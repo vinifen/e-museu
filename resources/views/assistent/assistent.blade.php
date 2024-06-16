ce90a604b8b71ba40aadfd8d049b55c36e99a6f7
tankesho <takeoga@hotmail.com>
2024-06-16 19:26:36 -0300

    Added small details in virtual assistent portrait

--- resources/views/assistent/assistent.blade.php
+++ resources/views/assistent/assistent.blade.php
@@ -3,7 +3,7 @@
         <div class="col-6 col-md-8">
             <div id="assistent-button" data-bs-toggle="collapse" data-bs-target="#assistentCollapse"
                 aria-expanded="false" aria-controls="assistentCollapse">
-                <img class="assistent-portrait mb-0" src="/img/assistent.png" alt="Assistente Virtual">
+                <img class="assistent-portrait mb-0" src="/img/assistent1.png" alt="Assistente Virtual">
             </div>
         </div>
     </div>
@@ -16,3 +16,14 @@
         <div id="choices" class="d-flex flex-column align-items-end my-2"></div>
     </div>
 </div>
+
+<Script>
+    $(document).ready(function() {
+        $('.assistent-portrait').on('click', function() {
+            const $img = $(this);
+            const currentSrc = $img.attr('src');
+
+            $img.attr('src', currentSrc.includes('/img/assistent1.png') ? '/img/assistent2.png' : '/img/assistent1.png');
+        });
+    });
+</script>
