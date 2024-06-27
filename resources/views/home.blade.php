60bebe7ecfaf359c5aef3e8fc48270e7d58185e9
Takeshi Ogassahara <86446096+tankesho@users.noreply.github.com>
2024-06-27 20:39:23 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,10 +64,10 @@
                     <p class="p-4">
                         A Universidade Tecnológica Federal do Paraná (UTFPR) e a Universidade Estadual do Centro-Oeste
                         (Unicentro) possuem projetos relacionados a lixo eletrônico. A <a
-                            href="https://www.utfpr.edu.br">UTFPR</a>
+                            href="https://www.utfpr.edu.br" target="_blank">UTFPR</a>
                         possui o projeto Tecno-Lixo:
                         Oficina do
-                        Aprender e a <a href="https://www3.unicentro.br">Unicentro</a> o projeto E-Lixo. Os projetos recebem
+                        Aprender e a <a href="https://www3.unicentro.br" target="_blank">Unicentro</a> o projeto E-Lixo. Os projetos recebem
                         peças de
                         computadores que as pessoas não
                         utilizam
